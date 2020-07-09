e9add3e7c8a166ff8141633d4ded070b9c245c84
steveclark91 <stephen.clark@colorado.edu>
2020-07-09 14:13:31 -0400

    Initial Commit

---
+++ Fixed Box in Center/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fixed Box in Center
+//
+//  Created by Stephen Clark on 7/9/20.
+//  Copyright © 2020 Stephen Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fixed Box in CenterTests/Fixed_Box_in_CenterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fixed_Box_in_CenterTests.swift
+//  Fixed Box in CenterTests
+//
+//  Created by Stephen Clark on 7/9/20.
+//  Copyright © 2020 Stephen Clark. All rights reserved.
+//
+
+import XCTest
+@testable import Fixed_Box_in_Center
+
+class Fixed_Box_in_CenterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
